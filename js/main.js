13abb34508e962ca095ee559d339cc5fe82628b0
Johan Castillo <jcjohan2707@gmail.com>
2020-09-03 14:18:35 -0400

    Obteniendo valores de los input con Vue.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,18 @@
 const app = new Vue({
   el: '#app',
   data: {
-    title: 'App with Vue.js'
+    title: 'App with Vue.js',
+    name: "",
+    pass: ""
   },
   methods: {
     hola(){
-      alert('Hola Mundo')
+      alert(this.pass)
     }
   }
 })
 
 /* var name = "johan"
-var pass = "12345"
-
-var inputName = document.getElementById('name')
-var passUser = document.getElementById('pass')
-
-var nameUser = inputName.value;
-
-//Detectar cambios en los inputs
-function a(){
-  alert(nameUser.value)
-}
-
-function b(){alert('B')}
-
-
 const redirect = () => {
   if(nameUser == name && passUser == pass){
     window.location.href = 'newPage.html'
