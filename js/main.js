08e49b74cfe7d8443d6f9d252f23a031597baca7
Johan Castillo <jcjohan2707@gmail.com>
2020-09-03 13:16:14 -0400

    Instalacion de Vue.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-const redirect = () => window.location.href = 'newPage.html';
+const app = new Vue({
+  el: '#app',
+  data: {
+    title: 'App with Vue.js'
+  },
+  methods: {
+    hola(){
+      alert('Hola Mundo')
+    }
+  }
+})
+
+/* var name = "johan"
+var pass = "12345"
+
+var inputName = document.getElementById('name')
+var passUser = document.getElementById('pass')
+
+var nameUser = inputName.value;
+
+//Detectar cambios en los inputs
+function a(){
+  alert(nameUser.value)
+}
+
+function b(){alert('B')}
+
+
+const redirect = () => {
+  if(nameUser == name && passUser == pass){
+    window.location.href = 'newPage.html'
+  }else{
+    alert('Datos incorrectos')
+  }
+};
+*/
